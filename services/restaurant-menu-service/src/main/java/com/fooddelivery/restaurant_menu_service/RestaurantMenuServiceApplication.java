02f8f9c5f892eaee2cdc310dbeafd2d196dc0b84
dyssos29 <o.filippidis@gmail.com>
2026-08-07 20:17:20 +0300

    chore(backend): create the two Spring Boot microservices

---
+++ services/restaurant-menu-service/src/main/java/com/fooddelivery/restaurant_menu_service/RestaurantMenuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.restaurant_menu_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMenuServiceApplication.class, args);
+	}
+
+}
